f5622128c81f1bcb806d4c9d9cf0aaab731ec2ac
shilkef1990 <31828604+shilkef1990@users.noreply.github.com>
2017-09-10 12:09:32 -0700

    Add files via upload
    First Submission

---
+++ M2T1.py
@@ -0,0 +1,14 @@
+# CTI-110
+# M2T1 - Sales Prediction
+# Frederick Shilke
+# 10 Sept 2017
+
+# Get the projected total sales.
+total_sales = float(input ('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
