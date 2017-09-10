3e7091437a0f178f1454c257d851740a7d287771
shilkef1990 <31828604+shilkef1990@users.noreply.github.com>
2017-09-10 16:23:12 -0700

    Add files via upload

---
+++ M2HW1_Shilke.py
@@ -0,0 +1,18 @@
+# M2HW1
+# Hello World
+# Frederick Shilke
+# 10 Sept 2017
+
+# Total estimated distance traveled.
+print (' Totatl estimated distance traveled. ')
+
+print (' 70mph * 6hrs = 420mi ')
+
+print (' 70mph * 10hrs = 700mi ')
+
+print (' 70mph * 15hrs = 1,050mi ')
+
+
+
+
+
---
+++ M2HW2_Shilke.py
@@ -0,0 +1,22 @@
+# M2HW1
+# Hello World
+# Frederick Shilke
+# 10 Sept 2017
+
+# Get the projected total sales.
+total_sales = float(input('Enter the cost of the meal purchased: ')) 
+
+# Calculate the tip for the meal.
+tip = total_sales * .18
+
+# Calculate the tax for the meal.
+tax = total_sales * .07
+
+# Display the tip.
+print('The tip is $', format(tip, ',.2f'))
+
+# Display the tax.
+print('The tax is $', format(tax, ',.2f'))
+
+
+
