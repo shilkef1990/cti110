34aab5956ba7079ad2378d0346b49cc5b9ff2c56
shilkef1990 <31828604+shilkef1990@users.noreply.github.com>
2017-09-22 21:39:17 -0700

    Add files via upload

---
+++ M3LAB_SHILKE.py
@@ -0,0 +1,43 @@
+# M3LAB
+# Hello World
+# Frederick Shilke
+# 23 Sept 2017
+
+def main():
+     print ()
+
+main()
+
+# TO DO: define the rest of the scores
+score = int(input('Enter grade: '))
+
+main()
+
+# system uses 10-point grading scale
+A_score = (90)
+B_score = (80)
+C_score = (70)
+
+# Determine the letter grade. 
+if score >= A_score:
+     print("Your grade is: A")
+
+elif score >= B_score:
+     print("Your grade is: B")
+
+elif score >= C_score:
+     print("Your grade is: C")
+
+else:
+     print('Your grade is: F')
+
+
+
+
+
+
+
+
+
+
+
